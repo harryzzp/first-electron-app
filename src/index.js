12131b92caf11efd4147516d17b8db4989ab209b
harryzzp <3123822>
2016-04-16 16:11:17 +0800

    angular

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-(function() {
-    var childProcess = require("child_process");
-    var oldSpawn = childProcess.spawn;
-    function mySpawn() {
-        console.log('spawn called');
-        console.log(arguments);
-        var result = oldSpawn.apply(this, arguments);
-        return result;
-    }
-    childProcess.spawn = mySpawn;
-})();
 
 window.$ = window.jQuery = require('./bower_components/jquery/dist/jquery.min.js')
 var fs = require('fs')
@@ -27,4 +16,7 @@ $(function () {
 
 })
 
+angular.module('first-electron-app', [])
+.controller('DemoCtrl', function($scope) {
+})
 
