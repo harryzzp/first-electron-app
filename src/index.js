e6338093cc20933150896783a3c55756cf879fc3
harryzzp <harryzzp@gmail.com>
2016-04-18 16:46:27 +0800

    calling　button

--- src/index.js
+++ src/index.js
@@ -6,19 +6,48 @@ var remote = require('remote');
 var dialog = remote.require('dialog')
 var fs = require('fs');
 var iconv_lite = require('iconv-lite')
+var cp = require('child_process');
+
+var command = 'spark-submit --class org.glsc.FullLog --master local[4] D:\\IdeaProjects\\LogAnalysis\\target\\LogAnalysis-1.0-SNAPSHOT.jar D:\\zzp 28018167'
 
 window.$ = window.jQuery = require('./bower_components/jquery/dist/jquery.min.js')
 
-var call_spark = require('./spark_exec.js')
+$(function () {
+  $('#button').click(function () {
 
-$(function() {
-  $('#button').click(function() {
-    alert('start');
-    call_spark.callSpark()
-    alert("It's me")
+    var ls = cp.exec(command, {
+      encoding: 'gbk',
+      timeout: 0,
+      maxBuffer: 20000 * 1024,
+      killSignal: 'SIGTERM',
+      cwd: null,
+      env: null
+    });
+
+    ls.stdout.on('data', function (data) {
+      str = iconv_lite.decode(data, 'gbk');
+      buf = iconv_lite.encode(str, 'utf8');
+      console.log('stdout: ' + buf);
+      fs.appendFile("result.log", buf, function (error) {
+        if (error) {
+          // 出现错误
+        }
+
+      });
+    });
+
+    ls.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
+
+    ls.on('exit', function (code) {
+      alert('运行结束！')
+      console.log('child process exited with code ' + code);
+    });
+
   })
 
-  $('#chooseFile').click(function() {
+  $('#chooseFile').click(function () {
     dialog.showOpenDialog({
       title: '选择文件',
       properties: ['openFile'],
@@ -26,8 +55,8 @@ $(function() {
         name: 'Text',
         extensions: ['txt', 'xml']
       }],
-    }, function(fileNames) {
-      if(fileNames) {
+    }, function (fileNames) {
+      if (fileNames) {
         var text = fs.readFileSync(fileNames[0]);
         //对GBK编码进行转换
         str = iconv_lite.decode(text, 'gbk');
@@ -39,5 +68,5 @@ $(function() {
 })
 
 angular.module('first-electron-app', [])
-.controller('DemoCtrl', function($scope) {
-})
+  .controller('DemoCtrl', function ($scope) {
+  })
--- src/spark_exec.js
+++ src/spark_exec.js
@@ -1,10 +1,72 @@
 var fs = require('fs')
 var iconv_lite = require('iconv-lite')
-
 var cp = require('child_process');
 
 var command = 'spark-submit --class org.glsc.FullLog --master local[4] D:\\IdeaProjects\\LogAnalysis\\target\\LogAnalysis-1.0-SNAPSHOT.jar D:\\zzp 28018167'
-//exec可以像spawn一样使用
+
+// function call() {
+//     cp.exec(command, {
+//     encoding: 'gbk',
+//     timeout: 0, 
+//     maxBuffer: 20000 * 1024,
+//     killSignal: 'SIGTERM',
+//     cwd: null,
+//     env: null
+// }, function(err, stdout, stderr){
+//     //stdout
+//     str = iconv_lite.decode(stdout, 'gbk');
+//     buf = iconv_lite.encode(str, 'utf8');
+//     console.log('stdout: ' + buf);
+//     fs.appendFile("result.log", buf, function (error) {
+//         if (error) {
+//             // 出现错误
+//         }
+
+//     });
+    
+//     //stderr
+//     console.log('stderr: ' + stderr);
+// });
+// }
+
+// var ls = cp.exec(command, {
+//     encoding: 'gbk',
+//     timeout: 0, 
+//     maxBuffer: 20000 * 1024,
+//     killSignal: 'SIGTERM',
+//     cwd: null,
+//     env: null
+// }, function(err, stdout, stderr){
+//     //stdout
+//     str = iconv_lite.decode(stdout, 'gbk');
+//     buf = iconv_lite.encode(str, 'utf8');
+//     console.log('stdout: ' + buf);
+//     fs.appendFile("result.log", buf, function (error) {
+//         if (error) {
+//             // 出现错误
+//         }
+
+//     });
+    
+//     //stderr
+//     console.log('stderr: ' + stderr);
+// });
+
+// ls.stdout.on('data', function (data) {
+//     str = iconv_lite.decode(data, 'gbk');
+//     buf = iconv_lite.encode(str, 'utf8');
+//     console.log('stdout: ' + buf);
+//     fs.appendFile("result.log", buf, function (error) {
+//         if (error) {
+//             // 出现错误
+//         }
+
+//     });
+// });
+
+// ls.stderr.on('data', function (data) {
+//     console.log('stderr: ' + data);
+// });
 
 var ls = cp.exec(command, {
     encoding: 'gbk',
