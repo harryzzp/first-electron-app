54e73cb42e1abd40cf8d1523cad8ba13f08f2670
harryzzp <3123822>
2016-04-18 21:54:15 +0800

    encoding

--- src/index.js
+++ src/index.js
@@ -8,12 +8,20 @@ var fs = require('fs');
 var iconv_lite = require('iconv-lite')
 var cp = require('child_process');
 
-var command = 'spark-submit --class org.glsc.FullLog --master local[4] D:\\IdeaProjects\\LogAnalysis\\target\\LogAnalysis-1.0-SNAPSHOT.jar D:\\zzp 28018167'
+var demo_text_path = ''
 
 window.$ = window.jQuery = require('./bower_components/jquery/dist/jquery.min.js')
 
 $(function () {
   $('#button').click(function () {
+    var num_of_cores = $('#num_of_cores').val()
+    var file_path = $('#file_path').val()
+    var search_text = $('#search_text').val()
+    var command = 'spark-submit --class org.glsc.FullLog --master local['
+      + num_of_cores + '] D:\\IdeaProjects\\LogAnalysis\\target\\LogAnalysis-1.0-SNAPSHOT.jar '
+      + file_path + ' ' + search_text
+
+    alert(command)
 
     var ls = cp.exec(command, {
       encoding: 'gbk',
@@ -48,27 +56,44 @@ $(function () {
     });
 
   })
-
+  
   $('#chooseFile').click(function () {
     dialog.showOpenDialog({
       title: '选择文件',
       properties: ['openFile'],
       filters: [{
         name: 'Text',
-        extensions: ['txt', 'xml']
+        extensions: ['txt', 'xml', 'log']
       }],
     }, function (fileNames) {
       if (fileNames) {
         var text = fs.readFileSync(fileNames[0]);
-        //对GBK编码进行转换
-        str = iconv_lite.decode(text, 'gbk');
+        // $('#output').text(text);
+        //对编码进行转换
+        str = iconv_lite.decode(text, $('#encoding_list option:selected').val());
         buf = iconv_lite.encode(str, 'utf8');
         $('#output').text(buf);
+        demo_text_path = fileNames;
       }
     })
   })
+  
 })
 
+function changeEncoding(sel) {
+  
+    if (demo_text_path != '') {
+        var text = fs.readFileSync(demo_text_path[0]);
+        // $('#output').text(text);
+        //对编码进行转换
+        str = iconv_lite.decode(text, sel.value);
+        buf = iconv_lite.encode(str, 'utf8');
+        $('#output').text('');
+        $('#output').text(buf);
+    }
+
+    }
+
 angular.module('first-electron-app', [])
-  .controller('DemoCtrl', function ($scope) {
+  .controller('ctrl', function ($scope) {
   })
