a7cf4ca5f3b9a185b5c10a53c16e16d432e29c7f
harryzzp <harryzzp@gmail.com>
2016-04-20 17:02:13 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,28 +2,73 @@
 //   alert("It's me");
 // })
 
-var remote = require('remote');
+var remote = require('remote')
 var dialog = remote.require('dialog')
-var fs = require('fs');
+var fs = require('fs')
 var iconv_lite = require('iconv-lite')
-var cp = require('child_process');
+var cp = require('child_process')
+var path = require('path')
 
-var demo_text_path = ''
+var demo_text_path = '';
 
 window.$ = window.jQuery = require('./bower_components/jquery/dist/jquery.min.js')
 
 $(function () {
-  $('#button').click(function () {
-    var master = $('#master').val()
-    var driver_memory = $('#driver_memory').val()
-    var serializer = $('#serializer').val()
-    var file_path = $('#file_path').val()
-    var search_text = $('#search_text').val()
-    var command = 'spark-submit --class org.glsc.FullLog --master '
-      + num_of_cores + ' D:\\IdeaProjects\\LogAnalysis\\target\\LogAnalysis-1.0-SNAPSHOT.jar '
-      + file_path + ' ' + search_text
+  $('#log_button').click(function () {
+    var log_dir = $('#log_dir').val().trim();
+    if (log_dir == '') {
+      alert('请输入日志路径！');
+      return;
+    }
+    var master = $('#master').val().trim();
+    if (master == '') {
+      alert('请设置spark.master！');
+      return;
+    }
+    var driver_memory = $('#driver_memory').val().trim();
+    if (driver_memory == '') {
+      alert('请分配内存！');
+      return;
+    }
+    var serializer = $('#spark_serializer').val().trim();
+    if (serializer == '') {
+      alert('请输入spark.serializer！');
+      return;
+    }
+    var search_text = $('#search_text').val().trim();
+    if (search_text == '') {
+      alert('请输入要搜索的内容！');
+      return;
+    }
+    if (!fs.existsSync('D:\\temp\\')) {
+      fs.mkdirSync('D:\\temp\\');
+    }
+    var tempFile = 'D:\\temp\\' + (new Date().getFullYear() + '.log');
 
-    alert(command)
+    if (fs.existsSync(tempFile)) {
+      fs.unlinkSync(tempFile);
+    }
+
+    if (!fs.existsSync(log_dir)) {
+      alert('日志目录' + log_dir + '不存在！');
+      return;
+    }
+
+    fs.readdirSync(log_dir).forEach((fileName) => {
+      var file = path.join(log_dir, fileName)
+      var text = fs.readFileSync(file);
+      fs.appendFileSync(tempFile, text);
+    });
+
+    var command = 'spark-submit --class org.glsc.FullLog --master '
+      + master + ' --driver-memory ' + driver_memory + ' '
+      + '--conf spark.eventLog.enabled=false '
+      + '--conf spark.serializer=org.apache.spark.serializer.KryoSerializer '
+      + '--conf spark.executor.extraJavaOptions="-XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:-UseGCOverheadLimit -XX:CMSInitiatingOccupancyFraction=10" '
+      + 'D:\\IdeaProjects\\LogAnalysis\\target\\LogAnalysis-1.0-SNAPSHOT.jar '
+      + '"' + tempFile + '" "' + search_text + '"';
+
+    alert("即将执行：" + command)
 
     var ls = cp.exec(command, {
       encoding: 'gbk',
@@ -35,21 +80,21 @@ $(function () {
     });
 
     ls.stdout.on('data', function (data) {
-      str = iconv_lite.decode(data, 'gbk');
-      buf = iconv_lite.encode(str, 'utf8');
-      // console.log('stdout: ' + buf);
-      // $('#log_output').text(buf);
-      $('#log_output').append('<p>' + buf + '</p>');
-      fs.appendFile("result.log", buf, function (error) {
-        if (error) {
-          // 出现错误
-        }
-
-      });
+      if (data.indexOf(search_text) != -1) {
+        str = iconv_lite.decode(data, 'gbk');
+        buf = iconv_lite.encode(str, 'utf8');
+        $('#log_output').append('<p>' + buf + '</p>');
+        fs.appendFile("result.log", buf, function (error) {
+          if (error) {
+            // 出现错误
+            $('#log_output').append('<p>' + buf + '</p>');
+          }
+        });
+      }
     });
 
     ls.stderr.on('data', function (data) {
-      // console.log('stderr: ' + data);
+      $('#log_output').append('<p>' + data + '</p>');
     });
 
     ls.on('exit', function (code) {
@@ -58,7 +103,7 @@ $(function () {
     });
 
   })
-  
+
   $('#chooseFile').click(function () {
     dialog.showOpenDialog({
       title: '选择文件',
@@ -79,22 +124,22 @@ $(function () {
       }
     })
   })
-  
+
 })
 
 function changeEncoding(sel) {
-  
-    if (demo_text_path != '') {
-        var text = fs.readFileSync(demo_text_path[0]);
-        // $('#output').text(text);
-        //对编码进行转换
-        str = iconv_lite.decode(text, sel.value);
-        buf = iconv_lite.encode(str, 'utf8');
-        $('#output').text('');
-        $('#output').text(buf);
-    }
 
-    }
+  if (demo_text_path != '') {
+    var text = fs.readFileSync(demo_text_path[0]);
+    // $('#output').text(text);
+    //对编码进行转换
+    str = iconv_lite.decode(text, sel.value);
+    buf = iconv_lite.encode(str, 'utf8');
+    $('#output').text('');
+    $('#output').text(buf);
+  }
+
+}
 
 angular.module('first-electron-app', [])
   .controller('ctrl', function ($scope) {
--- src/mixFiles.js
+++ src/mixFiles.js
@@ -1,23 +1,21 @@
 var fs = require('fs')
-var totalText;
-var srcFile;
+var path = require('path');
 
-// for (var i=1; i<=6; i++) {
-//     var fileName = 'D:\\logss\\wt001\\guolian.log.'+i;
-//     console.log(fileName)
-//     var text = fs.readFileSync(fileName);
-//     fs.appendFileSync('D:\\total.log', text)
-// }
+var start = new Date().getTime();
+var logDir = path.join('D:\\logss\\wt001\\');
 
-for (var i=0; i<=500; i++) {
-    var fileName
-    if (i==0) {
-       fileName  = 'D:\\logss\\newwt\\guolian.log';
-    } else {
-       fileName  = 'D:\\logss\\newwt\\guolian.log.'+i;
-    }
-    
-    console.log(fileName)
-    var text = fs.readFileSync(fileName);
-    fs.appendFileSync('D:\\3g.log', text)
-}
+// fs.exists(logDir, (exists) => {
+//     if (!exists) {
+//         console.log('Log directory [%s] does not exist.', logDir);
+//     } else {
+//         fs.readdirSync(logDir).forEach((fileName) => {
+//             var file = path.join(logDir, fileName)
+//             var text = fs.readFileSync(file);
+//             fs.appendFileSync('D:\\ccc.log', text);
+//             console.log(file);
+//         });
+//     }
+// })
+
+// var end = new Date().getTime()
+// console.log('Took %d ms.', (end - start));
